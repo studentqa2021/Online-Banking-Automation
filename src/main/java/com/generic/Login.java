2bd181828fa1eb09853f1a1f946f3108d8543224
studentqa2021 <sarowerhome@gmail.com>
2023-11-16 20:47:23 -0500

    fist commit

---
+++ src/main/java/com/generic/Login.java
@@ -0,0 +1,13 @@
+package com.generic;
+
+public class Login {
+	
+	int b=20;//name =b==> Global==> instance
+	static int c=40;// name =c ==> Global==> static 
+	
+	public static void main(String[] args) {
+		
+		int a=10;//v name =a ==> local
+	}
+
+}
